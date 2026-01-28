a531f90e5713596e864b43fa819a33d9f1ee205f
PhonePyaeKo <phonepyaeko01@gmail.com>
2026-01-28 20:53:01 +0630

    add Post Index and Store

--- app/Http/Controllers/Api/Posts/PostController.php
+++ app/Http/Controllers/Api/Posts/PostController.php
@@ -3,9 +3,51 @@
 namespace App\Http\Controllers\Api\Posts;
 
 use App\Http\Controllers\Controller;
+use App\Http\Helpers\ApiResponse;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    //
+    use ApiResponse;
+
+    //Index
+    public function index(Request $request)
+    {
+        $posts = Post::with('user')->latest()->paginate(5);
+
+        return $this->successResponse('Post Index', PostResource::collection($posts));
+    }
+
+    //Store
+    public function store(Request $request)
+    {
+        //Validate Form Data
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        //if validator fails
+        if($validator->fails()) {
+            return $this->errorResponse('Validation Error', $validator->errors(), 422);
+        }
+
+        try {
+            //create post
+            $post = Post::create([
+                'title' => $request->title,
+                'content' => $request->content,
+                'user_id' => Auth::id(),
+            ]);
+
+            return $this->errorResponse('Post Create Successfully', new PostResource($post->load('user')), 201);
+            
+        } catch (\Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500);
+        }
+    }
 }
